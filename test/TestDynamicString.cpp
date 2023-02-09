f4b7572f0c75facf008a23cccc171a81fc143e4e
Artem Suvorov <suvorov.artem@hotmail.com>
2023-02-09 21:25:53 +0500

    add more tests

--- test/TestDynamicString.cpp
+++ test/TestDynamicString.cpp
@@ -2,11 +2,51 @@
 
 #include "DynamicString.h"
 
-TEST(DynstrTest, DynamicString_IsEmpty_OnInit)
+TEST(DynstrTest, IsEmptyOnInit)
 {
     DynamicString string;
 
     EXPECT_STREQ(string.Characters(), "");
     EXPECT_EQ(string.Length(), 0);
     EXPECT_EQ(string.Capacity(), 1);
+}
+
+TEST(DynstrTest, AddsSingleCharacter)
+{
+    DynamicString string;
+    string.Add('A');
+
+    EXPECT_STREQ(string.Characters(), "A");
+    EXPECT_EQ(string.Length(), 1);
+    EXPECT_EQ(string.Capacity(), 2);
+}
+
+TEST(DynstrTest, ReallocatesMemory)
+{
+    DynamicString string;
+    string.Add('A');
+    string.Add('B');
+
+    EXPECT_STREQ(string.Characters(), "AB");
+    EXPECT_EQ(string.Length(), 2);
+    EXPECT_EQ(string.Capacity(), 4);
+}
+
+TEST(DynstrTest, CreateFromConstChar)
+{
+    DynamicString string = "Hello";
+    
+    EXPECT_STREQ(string.Characters(), "Hello");
+    EXPECT_EQ(string.Length(), 5);
+    EXPECT_EQ(string.Capacity(), 6);
+}
+
+TEST(DynstrTest, ModifiesCharacter)
+{
+    DynamicString string = "Henlo";
+    string[2] = 'l';
+    
+    EXPECT_STREQ(string.Characters(), "Hello");
+    EXPECT_EQ(string.Length(), 5);
+    EXPECT_EQ(string.Capacity(), 6);
 }
