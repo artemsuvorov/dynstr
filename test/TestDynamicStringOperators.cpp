8769438e458bd31340e52b0bc448cb7c6818eac2
Artem Suvorov <suvorov.artem@hotmail.com>
2023-02-12 04:16:41 +0500

    add more tests for the move assignment operator

--- test/TestDynamicStringOperators.cpp
+++ test/TestDynamicStringOperators.cpp
@@ -38,3 +38,35 @@ TEST(DynstrOperatorsTest, MoveAssignmentOperator)
     EXPECT_EQ(apple.Length(), 0);
     EXPECT_EQ(apple.Capacity(), 0);
 }
+
+TEST(DynstrOperatorsTest, MoveAssignmentOperator_WontMoveSelf)
+{
+    DynamicString apple = "Apple";
+    apple = std::move(apple);
+
+    EXPECT_NE(apple.Characters(), nullptr);
+
+    EXPECT_STREQ(apple.Characters(), "Apple");
+    EXPECT_EQ(apple.Length(), 5);
+    EXPECT_EQ(apple.Capacity(), 5);
+}
+
+TEST(DynstrOperatorsTest, MoveAssignmentOperator_MoveSameData)
+{
+    DynamicString apple1 = "Apple";
+    DynamicString apple2 = "Apple";
+
+    // different objects but same data
+    EXPECT_NE(&apple1, &apple2);
+
+    apple2 = std::move(apple1);
+
+    EXPECT_STREQ(apple2.Characters(), "Apple");
+    EXPECT_EQ(apple2.Length(), 5);
+    EXPECT_EQ(apple2.Capacity(), 5);
+
+    // expects apple1 string to become a hollow object
+    EXPECT_EQ(apple1.Characters(), nullptr);
+    EXPECT_EQ(apple1.Length(), 0);
+    EXPECT_EQ(apple1.Capacity(), 0);
+}
